084e10e4f210b14915624796f08c817d55ec31ed
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2024-10-10 20:13:22 +0800

    Add files via upload

---
+++ libs/codemirror.js
@@ -0,0 +1,10 @@
+﻿import CodeMirror from './codemirror/6.65.7/src/codemirror.js'
+import './codemirror/6.65.7/mode/xml/xml.js'
+import './codemirror/6.65.7/mode/javascript/javascript.js'
+import './codemirror/6.65.7/mode/css/css.js'
+import './codemirror/6.65.7/mode/htmlmixed/htmlmixed.js'
+import './codemirror/6.65.7/addon/search/search.js'
+import './codemirror/6.65.7/addon/search/searchcursor.js'
+import './codemirror/6.65.7/addon/dialog/dialog.js'
+
+export default CodeMirror
